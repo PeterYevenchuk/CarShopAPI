496ec676ab1d36f8cd430c13eca728b9fcd3e0eb
Peter <petroyevenchuk@gmail.com>
2023-06-29 13:35:01 +0300

    Add migration for db and fix db

---
+++ DAL/Db/CarsDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CarShop.Data_Access_Layer;
+
+public class CarsDbContext : DbContext
+{
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<CarPhoto> CarPhotos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source = cars.db");
+        optionsBuilder.ReplaceService<IMigrationsSqlGenerator, SqliteMigrationsSqlGenerator>();
+
+        //Command for terminal
+        //dotnet ef --project D:\MyCodeC#.NET\Git\CarShopAPI\DAL migrations add InitialMigration
+        //dotnet ef migrations remove
+    }
+}
--- DAL/Services/AdminService.cs
+++ DAL/Services/AdminService.cs
@@ -1,5 +1,4 @@
-﻿using DAL.Db;
-using DAL.Models;
+﻿using DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
